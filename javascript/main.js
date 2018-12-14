6b2783133810cf7696fa97def2ebd2d26171bfd2
jwhaeke <jwhaeke@live.nl>
2018-12-14 11:38:06 +0100

    Added icons to the screen which will link to different pages

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,22 @@
+//Future ideas -> When page has been visited before an returning to index.html
+//remove autoplay, show background still, and return with a waiting screen
+
+
+//After the background video has finished, show the screen and text
 function display() {
-    document.getElementById("hide1").style.display = "block";
     document.getElementById("hide2").style.display = "block";
     var vid = document.getElementById("loadScreen"); 
     vid.play(); 
-
     
 }
 
-// var vid = document.getElementById("loadScreen"); 
+var links = '<div class="icons"><a href="www.google.com"><i class="fas fa-user"></i></a><a href="www.google.com"><i class="fas fa-file-code"></i></a><a href="www.google.com"><i class="fas fa-power-off"></i></a></div>'
 
-// function playVid() { 
-// } 
+
+function projects() {
+    document.getElementById("hide1").style.display = "block";
+    document.getElementById("hide2").innerHTML = links;
+
+}
+
+
