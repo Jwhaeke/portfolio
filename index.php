2e7b1f35126ea3ce0d1f61e6adcd5607414cf47c
jwhaeke <jwhaeke@live.nl>
2019-01-21 15:34:39 +0100

    Linked all the pages together, and added a reboot button after the shut down video

--- index.php
+++ index.php
@@ -23,15 +23,6 @@ $visibility = "hidden";
     <title>Portfolio</title>
 </head>
 <body>
-
-    <!-- On load (maybe change to mouse over?) a bg video will load. on ended a "load screen" will appear.  -->
-    <!-- The screen will then show 3 icons (1st version) One for about me page, one for project page        -->
-    <!-- The last icon will trigger another bg video which will show me walking out the room and turning    -->
-    <!-- off the screen.                                                                                    -->
-    
-    <!-- <div id="forward">
-        <i class="fas fa-fast-forward font-weight-bold sticky-top"></i>
-    </div> -->
     
     <div id="hide2" class="screen">
         <div class="video">
@@ -69,10 +60,14 @@ $visibility = "hidden";
     </div>
 
     <div id="ender">
-            <video muted id="gone">
-                <source src="./vid/off.mp4" type="video/mp4">
-            </video>
-        </div>
+        <video muted id="gone" onended="reboot()">
+            <source src="./vid/off.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div id="reboot">
+    <a href="index.php"><img src="./img/icons8-refresh.svg"></a>
+    </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- javascript/main.js
+++ javascript/main.js
@@ -6,12 +6,12 @@
 
 function display() {
     document.getElementById("hide2").style.display = "block";
-    var vid = document.getElementById("loadScreen"); 
-    vid.play(); 
-    
+    var vid = document.getElementById("loadScreen");
+    vid.play();
+
 }
 
-var links = '<div class="icons"><a onmouseout="lightOff()" onmouseover="lightUp()" id="icon1" href="./pages/cv.html" target="_blank"><i class="fas fa-user"></i></a><a onmouseout="lightOff2()" onmouseover="lightUp2()" id="icon2" href="./pages/projects.html" target="_blank"><i class="fas fa-file-code"></i></a><a onmouseout="lightOff3()" onmouseover="lightUp3()" id="icon3" onclick="return false;" href=""><i onclick="walked()" class="fas fa-power-off"></i></a></div>'
+var links = '<div class="icons"><a onmouseout="lightOff()" onmouseover="lightUp()" id="icon1" href="./pages/cv.html"><i class="fas fa-user"></i></a><a onmouseout="lightOff2()" onmouseover="lightUp2()" id="icon2" href="./pages/projects.html"><i class="fas fa-file-code"></i></a><a onmouseout="lightOff3()" onmouseover="lightUp3()" id="icon3" onclick="return false;" href=""><i onclick="walked()" class="fas fa-power-off"></i></a></div>'
 
 function visited() {
     document.getElementById("hide2").style.display = "block";
@@ -53,24 +53,27 @@ function lightOff3() {
 
 }
 
-// var leave = '<div><video muted id="walk" onended="alert("mooh")><source src="./vid/leave.mp4" type="video/mp4"></video></div>';
 var leaveVid = './vid/leave.mp4'
 
 function walked() {
     document.getElementById("hide2").style.display = "none";
     document.getElementById("hide1").style.display = "none";
-    document.getElementById("bgvideo").style.display = "none";
-    console.log(document.getElementById("walkVid"));
+    if (document.getElementById('bgvideo')){
+        document.getElementById("bgvideo").style.display = "none";
+    }
     document.getElementById("walkVid").style.visibility = "visible";
-    var start = document.getElementById("walkVid"); 
-    start.play(); 
-    
+    var start = document.getElementById("walkVid");
+    start.play();
 }
-
 
 function showstuff() {
     document.getElementById("walkVid").style.visibility = "hidden";
     document.getElementById("gone").style.visibility = "visible";
-    var ending = document.getElementById("gone"); 
-    ending.play(); 
+    var ending = document.getElementById("gone");
+    ending.play();
+}
+
+function reboot() {
+    document.getElementById("reboot").style.visibility = "visible";
+    document.getElementById("reboot").style.position = "absolute";
 }
